6feaea91db5682f929b0aafb1f96e278b43851bf
Milo-Shen <Milo-Shen@outlook.com>
2022-10-16 22:46:35 +0800

    feat: add example for Simplify

---
+++ Typescript Best Practices/src/Simplify/main.ts
@@ -0,0 +1,12 @@
+// 巧用 Simplify
+
+// Pick 自己
+type Simplify<T> = Pick<T, keyof T>;
+
+type A = { a: string };
+type B = { b: number };
+type E = A & B;
+type F = Simplify<E>;
+
+function app(arg: E) { }
+function app_simplify(arg: F) { }
