b70bf74d4afe2d879aa0457837afb6987cafc718
Milo-Shen <Milo-Shen@outlook.com>
2022-10-16 22:24:01 +0800

    feat: add example for as const

--- Typescript Best Practices/src/As Const/main.ts
+++ Typescript Best Practices/src/As Const/main.ts
@@ -27,3 +27,39 @@ const fetchOptions2 = {
     credentials: 'include' as const,
 };
 fetch('/api', fetchOptions2);
+
+// 巧用 [a, b] as const
+// React.useState() 返回[state, setState] 的结构，方便调用方解构和命名：
+const [title, setTitle] = React.useState();
+
+// 这是一种很棒的设计，我们也效仿的话: 
+const makeGetSet = (initialValue: string) => {
+    let value = initialValue;
+    const setValue = (v: string) => value = v;
+    const getValue = () => value;
+    return [getValue, setValue];
+};
+
+const [getName, setName] = makeGetSet('14');
+// const currentName = getName(); // Error! But why?
+// 原因是 [0, ''] 会被推导为类型 (number | string)[] 。加 as const 可推断为元组: 
+
+const toGetSet = (initialValue: string) => {
+    let value = initialValue;
+    const setValue = (v: string) => value = v;
+    const getValue = () => value;
+    return [getValue, setValue] as const;
+};
+
+const [getName1, setName1] = toGetSet('14');
+const currentName = getName1(); // Great!
+
+// 业务中，自定义 hook 比较多用:
+const useFlag = (initialValue = false) => {
+    const [flag, setFlag] = React.useState(initialValue);
+    const up = React.useCallback(() => setFlag(true), []);
+    const down = React.useCallback(() => setFlag(false), []);
+    return [flag, up, down] as const;
+};
+
+const [modalVisible, showModal, hideModal] = useFlag();
