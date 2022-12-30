b76d8ed9a3f54ff2c0a2b8e9a3e14fb6b500d100
Milo-Shen <Milo-Shen@outlook.com>
2022-12-30 20:20:05 +0800

    feat: add good practice example

---
+++ Typescript Best Practices/src/Uppercase/main.ts
@@ -0,0 +1,7 @@
+type Role = 'admin' | 'user' | 'guest';
+
+// Bad practice
+type UppercaseRole_BAD = 'ADMIN' | 'USER' | 'GUEST';
+
+// Good practice
+type UppercaseRole = Uppercase<Role>; // "ADMIN" | "USER" | "GUEST"
