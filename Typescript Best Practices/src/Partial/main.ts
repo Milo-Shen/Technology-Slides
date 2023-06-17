10aefcd0091a994b5f195601a897de6b333364d4
Milo-Shen <Milo-Shen@outlook.com>
2023-06-17 21:21:10 +0800

    feat: add another example for Partial

--- Typescript Best Practices/src/Partial/main.ts
+++ Typescript Best Practices/src/Partial/main.ts
@@ -8,6 +8,18 @@ interface Props {
     name: string
 }
 
+// 将一个类型中的所有属性都变成可选属性
+let defaultState = {
+    foo: 7,
+    bar: 'hello'
+};
+
+type PartialState = Partial<typeof defaultState>;
+
+let partialState: PartialState = {
+    foo: 8
+};
+
 // class MyComponent extends React.PureComponent<Props> {
 //     defaultProps: Partial<Props> = {};
 // }
