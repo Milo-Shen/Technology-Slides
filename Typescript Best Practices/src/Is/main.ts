92aa0a7dd0313c1b4cc82fe83de934677a96d42f
Milo-Shen <Milo-Shen@outlook.com>
2023-06-18 17:11:02 +0800

    feat: update example in is example

--- Typescript Best Practices/src/Is/main.ts
+++ Typescript Best Practices/src/Is/main.ts
@@ -1,4 +1,5 @@
-// 类型收窄（Type guard）能帮我们更好地处理联合类型: 
+// 类型收窄（Type guard）能帮我们更好地处理联合类型:
+// 使用类型谓词 is 来实现之
 
 type Values = number[] | string[];
 const isNumberArray = (arr: unknown): arr is number[] => {
@@ -7,8 +8,10 @@ const isNumberArray = (arr: unknown): arr is number[] => {
 
 const handleValues = (values: Values) => {
     if (isNumberArray(values)) {
+        // 这个分支里的 value 为 number[];
         values.map(value => value.toFixed(2));
     } else {
+        // 这个分支里的 value 为 string[];
         values.map(value => value.length);
     }
 };
