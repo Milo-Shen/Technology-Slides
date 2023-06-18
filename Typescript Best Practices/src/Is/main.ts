1f67aa501894937edf2a16175eb6df08638f8650
Milo-Shen <Milo-Shen@outlook.com>
2023-06-18 19:52:20 +0800

    feat: add comments in is

--- Typescript Best Practices/src/Is/main.ts
+++ Typescript Best Practices/src/Is/main.ts
@@ -1,8 +1,11 @@
 // 类型收窄（Type guard）能帮我们更好地处理联合类型:
+
 // 使用类型谓词 is 来实现之
+// 所谓 is 关键字其实更多用在函数的返回值上，用来表示对于函数返回值的类型保护
 
 type Values = number[] | string[];
 const isNumberArray = (arr: unknown): arr is number[] => {
+    // 通常我们使用 is 关键字（类型谓词）在函数的返回值中，从而对于函数传入的参数进行类型保护
     return Array.isArray(arr) && arr.every(value => typeof value === 'number');
 };
 
