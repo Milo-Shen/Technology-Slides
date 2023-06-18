b2a99a2b75b7ceac88812ad3fbe5149be0ac9b0d
Milo-Shen <Milo-Shen@outlook.com>
2023-06-18 16:39:07 +0800

    feat: add forEach example for generic

--- Typescript Best Practices/src/Interface/main.ts
+++ Typescript Best Practices/src/Interface/main.ts
@@ -33,3 +33,20 @@ let value = getPersonValue(2);
 // 1. 当泛型出现在接口中时，比如 interface IPerson<T> 代表的是使用接口时需要传入泛型的类型，比如 IPerson<T>。
 // 2. 当泛型出现在接口内部时，比如第二个例子中的 IPerson2 接口代表一个函数，接口本身并不具备任何泛型定义。
 //    而接口代表的函数则会接受一个泛型定义。换句话说接口本身不需要泛型，而在实现使用接口代表的函数类型时需要声明该函数接受一个泛型参数。
+
+// 例子 3:
+// item 的类型取决于使用类型时传入的泛型参数
+type Callback<T> = (item: T) => void;
+
+// 在声明阶段就已经确定了 callback 接口中的泛型参数为外部传入的
+const forEach = <T>(arr: T[], callback: Callback<T>) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    callback(arr[i]);
+  }
+};
+
+// 自然，我们在调用forEach时显式声明泛型参数为 string | number 类型
+// 所以根据 forEach 的函数类型定义时，
+// 自然 callback 的 item 也会在定义时被推导为 T 也就是所谓的 string | number 类型
+forEach<string | number>(['1', 2, 3, '4'], (item) => {});
+// 所以，这一点在日常开发中希望小伙伴们一定要特别留意：在泛型接口中泛型的声明位置不同所产生的效果是完全不同的。
