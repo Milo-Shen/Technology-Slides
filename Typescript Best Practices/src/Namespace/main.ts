0f44fbd81ff009afa46b491eac51e33305082846
Milo-Shen <Milo-Shen@outlook.com>
2023-06-25 15:31:50 +0800

    feat: add external namespace example

--- Typescript Best Practices/src/Namespace/main.ts
+++ Typescript Best Practices/src/Namespace/main.ts
@@ -42,4 +42,30 @@ let sq = new polygons.Square();
 
 // 注意，我们并没有使用 require 关键字，而是直接使用导入符号的限定名赋值
 // 这与使用 var 相似，但它还适用于类型和导入的具有命名空间含义的符号
-// 重要的是，对于值来讲， import 会生成与原始符号不同的引用，所以改变别名的 var 值并不会影响原始变量的值
+// 重要的是, 对于值来讲, import 会生成与原始符号不同的引用，所以改变别名的 var 值并不会影响原始变量的值
+
+// 使用其它的 JavaScript 库
+// 为了描述不是用 TypeScript 编写的类库的类型，我们需要声明类库导出的 API。 由于大部分程序库只提供少数的顶级对象，命名空间是用来表示它们的一个好办法
+// 我们称其为声明是因为它不是外部程序的具体实现。 我们通常在 .d.ts 里写这些声明。 如果你熟悉 C/C++，你可以把它们当做 .h 文件。 让我们看一些例子
+
+// 外部命名空间
+// 流行的程序库 D3 在全局对象d3里定义它的功能。 因为这个库通过一个 <script> 标签加载（不是通过模块加载器），它的声明文件使用内部模块来定义它的类型。 为了让 TypeScript 编译器识别它的类型，我们使用外部命名空间声明。 比如，我们可以像下面这样写：
+declare namespace D3 {
+  export interface Selectors {
+    select: {
+      (selector: string): Selection;
+      (element: EventTarget): Selection;
+    };
+  }
+
+  export interface Event {
+    x: number;
+    y: number;
+  }
+
+  export interface Base extends Selectors {
+    event: Event;
+  }
+}
+
+declare var d3: D3.Base;
