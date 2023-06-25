c37e6ecf16fc466f9f03187202b3ece553523382
Milo-Shen <Milo-Shen@outlook.com>
2023-06-25 15:02:14 +0800

    feat: add Validation example for namespace

--- Typescript Best Practices/src/Namespace/main.ts
+++ Typescript Best Practices/src/Namespace/main.ts
@@ -1,24 +1,4 @@
 // 使用命名空间的验证器
-namespace Validation {
-  export interface StringValidator {
-    isAcceptable(s: string): boolean;
-  }
-
-  const lettersRegexp = /^[A-Za-z]+$/;
-  const numberRegexp = /^[0-9]+$/;
-
-  export class LettersOnlyValidator implements StringValidator {
-    isAcceptable(s: string) {
-      return lettersRegexp.test(s);
-    }
-  }
-
-  export class ZipCodeValidator implements StringValidator {
-    isAcceptable(s: string) {
-      return s.length === 5 && numberRegexp.test(s);
-    }
-  }
-}
 
 // Some samples to try
 let strings = ['Hello', '98052', '101'];
