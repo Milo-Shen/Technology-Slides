06ca4be1f486f697279f47175fe004d29a5e317a
Milo-Shen <Milo-Shen@outlook.com>
2023-06-25 21:37:20 +0800

    feat: add example in ClassOf

--- Typescript Best Practices/src/ClassOf/main.tsx
+++ Typescript Best Practices/src/ClassOf/main.tsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React from 'react';
 
 abstract class Animal extends React.PureComponent {
-    /* Common methods here. */
+  /* Common methods here. */
 }
-class Cat extends Animal { }
-class Dog extends Animal { }
+class Cat extends Animal {}
+class Dog extends Animal {}
 
 // `AnimalComponent` must be a class of Animal.
 const renderAnimal = (AnimalComponent: Animal) => {
-    // return <AnimalComponent />;        // WRONG!
-}
+  // return <AnimalComponent />;        // WRONG!
+};
 
 // 上面的代码是错的，因为 Animal 是实例类型，不是类本身。应该:
 interface ClassOf<T> {
-    new(...args: any[]): T;
-}
-const renderAnimal_1 = (AnimalComponent: ClassOf<Animal>) => {
-    return <AnimalComponent />; // Good!
+  new (...args: any[]): T;
 }
 
+type Constructor<T> = new (...args: any[]) => T;
+
+const renderAnimal_1 = (AnimalComponent: ClassOf<Animal>) => {
+  return <AnimalComponent />; // Good!
+};
+
+const renderAnimal_2 = (AnimalComponent: Constructor<Animal>) => {
+    return <AnimalComponent />; // Good!
+};
+
 renderAnimal_1(Cat); // Good!
-renderAnimal_1(Dog); // Good!
+renderAnimal_2(Dog); // Good!
