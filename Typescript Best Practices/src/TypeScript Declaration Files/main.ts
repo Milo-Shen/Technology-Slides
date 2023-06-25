bec7cf980c4018876d81d64fbf5f6c8dd66c1170
Milo-Shen <Milo-Shen@outlook.com>
2023-06-25 11:09:32 +0800

    feat: add declare module '*.png' example

--- Typescript Best Practices/src/TypeScript Declaration Files/main.ts
+++ Typescript Best Practices/src/TypeScript Declaration Files/main.ts
@@ -1,3 +1,5 @@
+// import logo from "./example.svg";
+
 // TypeScript 模块解析规则
 
 // TS 中的加载策略分为两种方式，分别为相对路径和绝对路径两种方式
@@ -44,12 +46,18 @@
 // 比如，在 TS 项目中我们需要引入一些后缀为 png 的图片资源，那么此时 TS 是无法识别此模块的
 // 解决方法也非常简单，通常我们会在项目的根目录中也就是和 tsconfig.json 平级的任意目录中添加对应的声明文件 image.d.ts:
 
-declare module '*.png' {
-  const src: string;
-  export default src;
-}
+// declare module '*.png' {
+//   const src: string;
+//   export default src;
+// }
 
 // 可以看到，通过定义声明文件的方式解决了我们的问题。
 // 可是，你有思考过按照上边的 typescript 对于模块的加载方式，它是怎么加载到我们声明的 image.d.ts 的吗 ？
 // 这是一个有意思的问题，按照上边我们提到的模块加载机制要么按照相对模块机制查找，要么按照对应的 node 模块解析机制进行查找
 // 怎么会查找到定义在项目目录中的 image.d.ts 呢 ？
+
+// 本质上我们引入任何模块时，加载机制无非就是我们上边提到的两种加载方式。
+// 不过，这里有一个细小的点即是 ts 编译器会处理 tsconfig.json 的 file、include、exclude 对应目录下的所有 .d.ts 文件：
+// 简单来说，ts 编译器首先会根据 tsconfig.json 中的上述三个字段来加载项目内的 d.ts 全局模块声明文件，自然由于 '.png' 文件会命中全局加载的 image.d.ts 中的 声明的 module 所以会找到对应的文件
+// include 在未指定 file 配置下默认为 **，表示 tsc 解析的目录为当前 tsconfig.json 所在的项目文件夹
+// 关于 file、include、exclude 三者的区别我就不详细展开了，本质上都是针对于 TSC 编译器处理的范围。后续如果大伙有兴趣，我可以单独开一个 tsconfig.json 的文章去详细解释配置
