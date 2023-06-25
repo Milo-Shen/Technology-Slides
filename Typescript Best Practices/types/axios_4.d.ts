af6c29450988a2d63923479fe64dcc9104a8c934
Milo-Shen <Milo-Shen@outlook.com>
2023-06-25 22:24:04 +0800

    feat: add export = example

--- Typescript Best Practices/types/axios_4.d.ts
+++ Typescript Best Practices/types/axios_4.d.ts
@@ -10,4 +10,4 @@ import axios = require('axios');
 // 可以看到上述的代码，我们通过 export = axios 定义了一个相关的 CJS 模块语法
 // 需要额外注意的是在 ts 中若要导入一个使用了export = 的模块时，必须使用 TypeScript 提供的特定语法 import module = require("module")
 // 在日常业务中，不可避免我们会碰到一些相关 commonjs 规范语法的模块，那么当我们需要扩充对应的模块或者为该模块声明定义文件时，就需要使用到上述的 export = 这种语法了
-// 当然，export = 这种语法不仅仅可以支持 cjs 模块。它也同样是 ts 为了 ADM 提出的模块兼容声明。有兴趣的朋友可以详细查阅官方文档
+// 当然，export = 这种语法不仅仅可以支持 cjs 模块。它也同样是 TS 为了 ADM 提出的模块兼容声明。有兴趣的朋友可以详细查阅官方文档
